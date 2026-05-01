ab5cebf889d66b541b27a5335b33370a9a811650
Jayarani05 <jayaranimanimaran13@gmail.com>
2026-05-01 21:44:35 +0530

    Initial Spring Boot setup for microservices architecture

---
+++ src/main/java/com/example/realtime_ecommerce_microservices_platform/RealtimeEcommerceMicroservicesPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.realtime_ecommerce_microservices_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimeEcommerceMicroservicesPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtimeEcommerceMicroservicesPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/realtime_ecommerce_microservices_platform/RealtimeEcommerceMicroservicesPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.realtime_ecommerce_microservices_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimeEcommerceMicroservicesPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
