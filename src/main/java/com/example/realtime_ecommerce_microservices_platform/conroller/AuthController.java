6dff53089edd5a7477d1cc329c67671af49bcb8b
Jayarani05 <jayaranimanimaran13@gmail.com>
2026-05-02 21:34:51 +0530

    post registeration in controller added

--- src/main/java/com/example/realtime_ecommerce_microservices_platform/conroller/AuthController.java
+++ src/main/java/com/example/realtime_ecommerce_microservices_platform/conroller/AuthController.java
@@ -1,5 +1,24 @@
 package com.example.realtime_ecommerce_microservices_platform.conroller;
 
+import com.example.realtime_ecommerce_microservices_platform.dtos.AuthResponse;
+import com.example.realtime_ecommerce_microservices_platform.dtos.RegisterRequest;
+import com.example.realtime_ecommerce_microservices_platform.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public AuthResponse register(@RequestBody RegisterRequest request) {
+        return new AuthResponse(authService.register(request));
+    }
 
 }
