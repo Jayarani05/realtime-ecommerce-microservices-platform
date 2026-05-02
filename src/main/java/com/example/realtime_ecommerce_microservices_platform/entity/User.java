9ef1714af9af6b5951cc4887fe677244ad91263b
Jayarani05 <jayaranimanimaran13@gmail.com>
2026-05-02 21:10:45 +0530

    User entity added sucessfully

---
+++ src/main/java/com/example/realtime_ecommerce_microservices_platform/entity/User.java
@@ -0,0 +1,24 @@
+package com.example.realtime_ecommerce_microservices_platform.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+
+@Entity
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private String role;
+
+}
