7825d8934c74bb8f97f2c6eea4a1a23c183714da
Jayarani05 <jayaranimanimaran13@gmail.com>
2026-05-03 21:34:17 +0530

    CustomUserDetailsService added in security

---
+++ src/main/java/com/example/realtime_ecommerce_microservices_platform/security/CustomUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.example.realtime_ecommerce_microservices_platform.security;
+
+import com.example.realtime_ecommerce_microservices_platform.Repository.UserRepository;
+import com.example.realtime_ecommerce_microservices_platform.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                List.of(new SimpleGrantedAuthority(user.getRole()))
+        );
+    }
+}
