ebb532390a6703f8d8cab55466974b275466d839
Jayarani05 <jayaranimanimaran13@gmail.com>
2026-05-18 21:19:28 +0530

    test controller to check the role based authentication

---
+++ src/test/java/com/example/realtime_ecommerce_microservices_platform/testcontroller/TestController.java
@@ -0,0 +1,18 @@
+package com.example.realtime_ecommerce_microservices_platform.testcontroller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/user/dashboard")
+    public String userDashboard() {
+        return "User dashboard";
+    }
+
+    @GetMapping("/admin/dashboard")
+    public String adminDashboard() {
+        return "Admin dashboard";
+    }
+}
